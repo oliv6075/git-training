c0ddf8fadb177108a2c2cbe6cfc86d6df9466c2a
oliv6075 <oliverovergaardolesen@gmail.com>
2024-11-10 15:01:05 +0000

    cdecs

--- test_packages.R
+++ test_packages.R
@@ -3,8 +3,8 @@
 # Load libraries
 library(ggplot2)
 
-# Generate a modified plot
-plot_data <- data.frame(x = 1:10, y = (1:10)^3)
+# Generate a modified plot in main
+plot_data <- data.frame(x = 1:10, y = (1:10)^2)
 ggplot(plot_data, aes(x = x, y = y)) +
-  geom_line(color = "blue") +
-  ggtitle("Modified Plot in Feature Branch")
+  geom_line(color = "red") +
+  ggtitle("Modified Plot in Main Branch")
