9782cf99aa7ac7649244444037fb615e17592f2d
oliv6075 <oliverovergaardolesen@gmail.com>
2024-11-10 15:06:17 +0000

    ewsc

--- test_packages.R
+++ test_packages.R
@@ -3,8 +3,8 @@
 # Load libraries
 library(ggplot2)
 
-# Generate a modified plot in main
+# Generate a basic plot
 plot_data <- data.frame(x = 1:10, y = (1:10)^2)
 ggplot(plot_data, aes(x = x, y = y)) +
-  geom_line(color = "red") +
-  ggtitle("Modified Plot in Main Branch")
+  geom_line() +
+  ggtitle("Initial Plot in Main")
