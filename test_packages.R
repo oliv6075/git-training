2a8d3fe2b83b1c14f01109b3ed30be2a6f798b64
oliv6075 <oliverovergaardolesen@gmail.com>
2024-11-10 14:57:05 +0000

    desa

--- test_packages.R
+++ test_packages.R
@@ -3,8 +3,8 @@
 # Load libraries
 library(ggplot2)
 
-# Generate a basic plot
-plot_data <- data.frame(x = 1:10, y = (1:10)^2)
+# Generate a modified plot
+plot_data <- data.frame(x = 1:10, y = (1:10)^3)
 ggplot(plot_data, aes(x = x, y = y)) +
-  geom_line() +
-  ggtitle("Initial Plot")
+  geom_line(color = "blue") +
+  ggtitle("Modified Plot in Feature Branch")
