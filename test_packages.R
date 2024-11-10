9ca67738631064d7004628b62874a701ba1fffa5
oliv6075 <oliverovergaardolesen@gmail.com>
2024-11-10 15:16:56 +0000

    cde

--- test_packages.R
+++ test_packages.R
@@ -6,5 +6,5 @@ library(ggplot2)
 # Generate a basic plot with a new title in main
 plot_data <- data.frame(x = 1:10, y = (1:10)^2)
 ggplot(plot_data, aes(x = x, y = y)) +
-  geom_line(color = "red") +
+  geom_line(color = "red") 
   ggtitle("Conflicting Title in Main Branch")
