f4f20235dc1b1d18a5aeff3d46a28bb21978bc3e
oliv6075 <oliverovergaardolesen@gmail.com>
2024-11-10 15:17:05 +0000

    csa

--- test_packages.R
+++ test_packages.R
@@ -4,7 +4,7 @@
 library(ggplot2)
 
 # Generate a basic plot with a new title in main
-plot_data <- data.frame(x = 1:10, y = (1:10)^2)
-ggplot(plot_data, aes(x = x, y = y)) +
-  geom_line(color = "red") 
-  ggtitle("Conflicting Title in Main Branch")
+plot_data <- daxasxta.frame(x = 1:10, y = (1:10)^2)
+ggplot(otsa_daata, aes(x = x, y = y)) +
+  geom_line(col = "red") 
+  ggtitle("Con<<flicting Title in Main Branch")
