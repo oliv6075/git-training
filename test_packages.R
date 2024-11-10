1db30f412b1200b25d5ee0face4cdff0f30e8928
oliv6075 <oliverovergaardolesen@gmail.com>
2024-11-10 15:10:16 +0000

    dsa

--- test_packages.R
+++ test_packages.R
@@ -3,8 +3,8 @@
 # Load libraries
 library(ggplot2)
 
-# Generate a basic plot
+# Generate a basic plot with a new title in main
 plot_data <- data.frame(x = 1:10, y = (1:10)^2)
 ggplot(plot_data, aes(x = x, y = y)) +
-  geom_line() +
-  ggtitle("Initial Plot in Main")
+  geom_line(color = "red") +
+  ggtitle("Conflicting Title in Main Branch")
